4edbeefae3d749fadb9c56c82d2ab0f382bd9b8a
Steve Wood <steve@wood-family.net>
2017-08-15 18:07:23 -0500

    GTalk

---
+++ Software Version Scripts/gtalkVersionPlist-public.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Name: gtalkVersionPlist-public.sh
+# Date: 15 Aug 2017
+# Author: Steve Wood (steve.wood@omnicomgroup.com)
+# Purpose: checks the version of Google Talk
+
+plistPath='/path/to/plist/com.contoso.swversions.plist'
+
+if [[ -d "/Library/Internet\ Plug-Ins/googletalkbrowserplugin.plugin" ]]; then
+
+	gtalkVersion=`defaults read /Library/Internet\ Plug-Ins/googletalkbrowserplugin.plugin/Contents/Info CFBundleShortVersionString`
+	defaults write ${plistPath} GTalkVersion -string ${gtalkVersion}
+	
+else
+	
+	defaults write ${plistPath} GTalkVersion -string "Not Installed"
+	
+fi
+
